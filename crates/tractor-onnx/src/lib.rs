12ee906fbc69100b34dff4f0da7760678bc4f2d6
Tom Solberg <me@sbg.dev>
2022-05-12 00:03:20 +0200

    cleanup onnx

--- crates/tractor-onnx/src/lib.rs
+++ crates/tractor-onnx/src/lib.rs
@@ -1,18 +1,20 @@
-/// Contains utilities for interacting with ONNX specifically.
+/// Contains utilities for using tractor with ONNX.
+use anyhow::Result;
 use std::io::Read;
 use tract_onnx::WithOnnx;
+use tract_onnx::{prelude::*, tract_hir::infer::Factoid};
 use tractor::{BasicInferer, DynamicBatchingInferer, FixedBatchingInferer};
 
-use anyhow::Result;
-use tract_onnx::{prelude::*, tract_hir::infer::Factoid};
+fn model_for_reader(reader: &mut dyn Read) -> Result<InferenceModel> {
+    let onnx = tract_onnx::onnx();
+    onnx.model_for_read(reader)
+}
 
 /// Create a basic inferer from the provided bytes reader.
 ///
 /// See [`BasicInferer`] for more details.
 pub fn simple_inferer_from_stream(reader: &mut dyn Read) -> Result<BasicInferer> {
-    let onnx = tract_onnx::onnx();
-    let model = onnx.model_for_read(reader)?;
-
+    let model = model_for_reader(reader)?;
     BasicInferer::from_model(model)
 }
 
@@ -23,33 +25,28 @@ pub fn batched_inferer_from_stream(
     reader: &mut dyn Read,
     batch_size: &[usize],
 ) -> Result<DynamicBatchingInferer> {
-    let onnx = tract_onnx::onnx();
-    let model = onnx.model_for_read(reader)?;
-
+    let model = model_for_reader(reader)?;
     DynamicBatchingInferer::from_model(model, batch_size)
 }
 
 /// Create an fixed batching inferer from the provided bytes reader
 ///
-/// See [`DynamicBatchingInferer`] for more details.
+/// See [`FixedBatchingInferer`] for more details.
 pub fn fixed_batch_inferer_from_stream(
     reader: &mut dyn Read,
     batch_size: &[usize],
 ) -> Result<FixedBatchingInferer> {
-    let onnx = tract_onnx::onnx();
-    let model = onnx.model_for_read(reader)?;
-
+    let model = model_for_reader(reader)?;
     FixedBatchingInferer::from_model(model, batch_size)
 }
 
 /// Convert an ONNX model to a NNEF model.
 pub fn to_nnef(reader: &mut dyn Read) -> Result<Vec<u8>> {
-    let onnx = tract_onnx::onnx();
+    let mut model = model_for_reader(reader)?;
 
-    let mut model = onnx.model_for_read(reader)?;
     let batch = Symbol::new('N');
-
     let input_outlets = model.input_outlets()?.to_vec();
+
     for input_outlet in input_outlets {
         let input_shape = &model.input_fact(input_outlet.node)?.shape;
         let mut shape: Vec<_> = input_shape
@@ -66,10 +63,11 @@ pub fn to_nnef(reader: &mut dyn Read) -> Result<Vec<u8>> {
         )?;
     }
 
-    let mut model = model.into_typed()?;
-    model.declutter()?;
+    let model = model.into_typed()?.into_decluttered()?;
+
     let mut output = vec![];
     let nnef = tract_nnef::nnef().with_tract_core().with_onnx();
+
     nnef.write(&model, &mut output)?;
     Ok(output)
 }
