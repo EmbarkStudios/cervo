263304935d0aa42438ec9972844fe623c0b93e79
Tom Solberg <me@sbg.dev>
2022-05-18 11:06:55 +0200

    add lib with re-exports to cervo bin (#11)

---
+++ crates/cervo/src/lib.rs
@@ -0,0 +1,7 @@
+/// Re-exports all subcrates without the prefixes.
+pub mod prelude {
+    pub use cervo_asset as asset;
+    pub use cervo_core as core;
+    pub use cervo_nnef as nnef;
+    pub use cervo_onnx as onnx;
+}
