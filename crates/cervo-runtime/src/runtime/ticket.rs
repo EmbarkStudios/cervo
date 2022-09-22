604d6901e08784f46ef87464776fc302c7282c99
Tom Solberg <me@sbg.dev>
2022-09-22 15:30:01 +0200

    more tests

--- crates/cervo-runtime/src/runtime/ticket.rs
+++ crates/cervo-runtime/src/runtime/ticket.rs
@@ -24,3 +24,36 @@ impl Ord for Ticket {
         self.partial_cmp(other).unwrap()
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use std::cmp::Ordering;
+
+    use super::{BrainId, Ticket};
+    #[test]
+    fn ticket_cmp_is_inversed() {
+        let a = Ticket(0, BrainId(0));
+        let b = Ticket(1, BrainId(1));
+
+        // a has lower sequence number = higher sorting power
+        assert_eq!(a.cmp(&b), Ordering::Greater);
+    }
+
+    #[test]
+    fn ticket_cmp_ignore_brain() {
+        let a = Ticket(0, BrainId(1));
+        let b = Ticket(1, BrainId(0));
+
+        // a has lower sequence number = higher sorting power
+        assert_eq!(a.cmp(&b), Ordering::Greater);
+    }
+
+    #[test]
+    fn ticket_cmp_is_inversed_reverse_cmp() {
+        let a = Ticket(0, BrainId(0));
+        let b = Ticket(1, BrainId(1));
+
+        // a has lower sequence number = higher sorting power, so b is less.
+        assert_eq!(b.cmp(&a), Ordering::Less);
+    }
+}
