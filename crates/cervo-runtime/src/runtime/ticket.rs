2c75f466bff495e5feb75f3e2b1306a7d540b330
Tom Solberg <me@sbg.dev>
2023-10-09 12:26:46 +0200

    fix clippy

--- crates/cervo-runtime/src/runtime/ticket.rs
+++ crates/cervo-runtime/src/runtime/ticket.rs
@@ -16,13 +16,13 @@ impl Eq for Ticket {}
 
 impl PartialOrd for Ticket {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        other.0.partial_cmp(&self.0)
+        Some(self.cmp(other))
     }
 }
 
 impl Ord for Ticket {
     fn cmp(&self, other: &Ticket) -> Ordering {
-        self.partial_cmp(other).unwrap()
+        other.0.cmp(&self.0)
     }
 }
 
